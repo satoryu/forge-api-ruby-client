f8478bb1f42fe35d00e6eb675d07b8386f897b12
Tatsuya Sato <sato.tatsuya@hololab.co.jp>
2022-11-17 11:57:09 +0900

    Define manifest API

--- lib/autodesk/forge/model_derivative/manifest.rb
+++ lib/autodesk/forge/model_derivative/manifest.rb
@@ -1,7 +1,18 @@
+require 'autodesk/forge/api'
+
 module Autodesk
   class Forge
     module ModelDerivative
-      class Manifest
+      class Manifest < Forge::API
+        def initialize(credentials:)
+          @credentials = credentials
+        end
+
+        def get(urn)
+          response = get_request("/modelderivative/v2/designdata/#{URI.encode_www_form_component(urn)}/manifest")
+
+          JSON.parse(response.body)
+        end
       end
     end
   end
--- spec/autodesk/forge/model_derivative/manifest_spec.rb
+++ spec/autodesk/forge/model_derivative/manifest_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 RSpec.describe Autodesk::Forge::ModelDerivative::Manifest do
+  let(:urn) { 'dummy_Urn' }
 
+  it 'sends a request to the endpoint' do
+    stub_hubs = stub_request(:get, "https://developer.api.autodesk.com/modelderivative/v2/designdata/#{URI.encode_www_form_component(urn)}/manifest")
+      .with(headers: { Authorization: 'Bearer dummy-access-token'})
+      .to_return({ body: { data: 'dummy-data' }.to_json })
+
+    credentials = { 'access_token' => 'dummy-access-token' }
+
+    manifest = Autodesk::Forge::ModelDerivative::Manifest.new(credentials: credentials)
+    manifest = manifest.get(urn)
+
+    expect(stub_hubs).to have_been_requested
+    expect(manifest['data']).to be_eql('dummy-data')
+  end
 end

68c2e7862c84a044bf9331bc292ec8fead6ebd77
mya-zaki <gota.miyazaki@gmail.com>
2023-04-13 17:24:34 +0900

    fix: parse response properties api

--- lib/autodesk/forge/acc_model_properties/query.rb
+++ lib/autodesk/forge/acc_model_properties/query.rb
@@ -25,8 +25,8 @@ module Autodesk
 
         def properties
           response = get_request("/construction/index/v2/projects/#{@project_id}/indexes/#{@index_id}/queries/#{@query_id}/properties")
-
-          JSON.parse(response.body)
+          data = "{ \"data\": [#{response.body.gsub(/\n/, ',')}]}"
+          JSON.parse(data)
         end
 
       end
